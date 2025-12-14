57607449d5c7201b09eae06cd30443196d0a8edb
George H <george.lc.hewlett@gmail.com>
2025-12-14 16:59:51 +0000

    Updated `PostPaymentedRequested.cs` naming and variable type also implmeneted data annotations.

--- src/PaymentGateway.Api/Models/Requests/PostPaymentRequest.cs
+++ src/PaymentGateway.Api/Models/Requests/PostPaymentRequest.cs
@@ -1,11 +1,27 @@
-﻿namespace PaymentGateway.Api.Models.Requests;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PaymentGateway.Api.Models.Requests;
 
 public class PostPaymentRequest
 {
-    public int CardNumberLastFour { get; set; }
+    [Required]
+    public string CardNumber { get; set; } = string.Empty;
+
+    [Required]
+    [Range(1, 12)]
     public int ExpiryMonth { get; set; }
+
+    [Required]
     public int ExpiryYear { get; set; }
-    public string Currency { get; set; }
+
+    [Required]
+    [StringLength(3, MinimumLength = 3)]
+    public string Currency { get; set; } = string.Empty;
+
+    [Required]
     public int Amount { get; set; }
-    public int Cvv { get; set; }
+
+    [Required]
+    [StringLength(4, MinimumLength = 3)]
+    public string Cvv { get; set; } = string.Empty;
 }
